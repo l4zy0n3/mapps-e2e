0041164c7f5322adbb562e20604b32b59332f5b6
Yash Tripathi <yash.tripathi@suse.com>
2025-02-05 14:46:35 +0530

    Added repository specific tests

--- cypress/e2e/repositories.spec.cy.js
+++ cypress/e2e/repositories.spec.cy.js
@@ -7,21 +7,38 @@ describe('Repository CRUD Operations', () => {
   let repoPage;
   let sidebar;
   let loginPage;
+  let repoName = '';
 
   beforeEach(() => {
     cy.visit('http://rancher.local');
     loginPage = new LoginPage();
     sidebar = new SideBar();
     repoPage = new RepositoriesPage();
-
-    cy.login('admin', 'mytestcluster');
+    const username = Cypress.env('RANCHER_USERNAME') || 'admin';
+    const password = Cypress.env('RANCHER_PASSWORD') || 'mytestcluster';
+    loginPage.login(username, password);
     sidebar.navigateToReposPage();
+    repoName = generateName('test-repo');
+    repoPage.addRepo(repoName, 'https://git.rancher.io/charts', 'git', 'release-v2.10');
   });
 
-  it('should perform repo CRUD operations', () => {
-    const repoName = generateName('test-repo');
-    repoPage.addRepo(repoName, 'https://git.rancher.io/charts', 'git', 'release-v2.10');
-    // repoPage.editRepo(repoName); // Implement this method in RepositoriesPage if needed
-    // repoPage.deleteRepo(repoName); // Implement this method in RepositoriesPage if needed
+  it('User should not be able to edit the repo name after creation', () => {
+    repoPage.searchRepo(repoName);
+    repoPage.RepoNameLink.click();
+    this.ActionMenu.click()
+    cy.contains('Edit Config').click() 
+    repoPage.RepoNameInput.should('be.disabled');
   });
+
+  it('User should be able to clone the repo', () => {
+    repoPage.searchRepo(repoName);
+    repoPage.RepoNameLink.click();
+    repoPage.ActionMenu.click()
+    cy.contains('Clone').click() 
+    cy.get('[placeholder="A unique name"]').type(repoName+'-clone')
+    repoPage.CreateButton.click()
+    repoPage.searchRepo(repoName+'-clone');
+    cy.verifyRegexDoesExist(`Active.*${repoName+'-clone'}`);
+  });
+
 }); 
